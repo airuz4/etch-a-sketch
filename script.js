2993395b48aff0021ef4c33852a46c4bb495b5f2
Airuz Balungay <balungay.airuz@gmail.com>
2026-07-29 13:39:20 +0800

    fully functional, with user input checking handling

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 let container = document.querySelector('.container');
 let containerSize  = 500;
 container.style.width = `${containerSize}px`;
@@ -38,8 +30,10 @@ container.addEventListener('mouseover',(e) => {
 let sizeBtn = document.querySelector('#sizeBtn');
 
 sizeBtn.addEventListener(('click'), () => {
-    while(container.firstChild)
-        container.removeChild(container.firstChild);
     let input = Number(prompt("Enter a number from 1 - 100", ''));
-    createGrid(input);
+    if (isNaN(input) || input > 100 || input < 0){
+            alert('enter a valid value');
+            return;
+        }
+        createGrid(input);
 })
