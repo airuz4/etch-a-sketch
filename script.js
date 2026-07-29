a1d619273fd2245b3da2f86a62dedb8dd4de1c47
Airuz Balungay <balungay.airuz@gmail.com>
2026-07-29 13:31:48 +0800

    mission accomplished, added no. of grids

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 
-let container = document.querySelector('.container');
-let boxSize = 20;
 
-function createBox() {
-    let box = document.createElement('div');
-    box.setAttribute('class','box');
-    return box;
-}
+
+
+
+
+
+
+let container = document.querySelector('.container');
+let containerSize  = 500;
+container.style.width = `${containerSize}px`;
+container.style.height = `${containerSize}px`;
 
 
 function createGrid(number){
-    let gridSize = number * boxSize;
-    container.style.width = `${gridSize}px`;
-    container.style.height = `${gridSize}px`;
-
     let boxCount = number ** 2;
-        for (let width = 0; width < boxCount; width++){
-            container.appendChild(createBox());
+        for (let i = 0; i < boxCount; i++){
+            let box = document.createElement('div');
+            box.setAttribute('class','box');
+            box.style.flex = `1 1 ${containerSize / number - 2}px`
+            container.appendChild(box);
         }
 }
 
-createGrid(4);
+
 
 function boxBlack(box) {
-    box.setAttribute('style', 'background-color: black');
+    box.style.backgroundColor = 'black';
 }
 
 container.addEventListener('mouseover',(e) => {
@@ -32,3 +34,12 @@ container.addEventListener('mouseover',(e) => {
         boxBlack(target);
     }
 });
+
+let sizeBtn = document.querySelector('#sizeBtn');
+
+sizeBtn.addEventListener(('click'), () => {
+    while(container.firstChild)
+        container.removeChild(container.firstChild);
+    let input = Number(prompt("Enter a number from 1 - 100", ''));
+    createGrid(input);
+})
